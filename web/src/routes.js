d751eec4009ce9dee29ec473a14f96af23dfdc79
Daniel <daniel.brz2009@gmail.com>
2021-01-17 04:27:01 -0300

    mdbreact adicionado

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route />
+        <Route path="/" component={Home} />
       </Switch>
     </BrowserRouter>
   );
