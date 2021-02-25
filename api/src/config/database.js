f6ec4acbe5b4302b44adadfb307215f38e80f1f6
daniel <daniel.brz2009@gmail.com>
2021-02-25 00:06:30 -0300

    sei la

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,9 +1,29 @@
+// module.exports = {
+//   dialect: 'postgres',
+//   host: 'localhost',
+//   username: 'postgres',
+//   password: 'mesavip',
+//   database: 'mesavip',
+//   define: {
+//     timestamps: true,
+//     undescored: true,
+//     underscoredAll: true,
+//   },
+// };
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'mesavip',
-  database: 'mesavip',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  host: 'ec2-54-87-34-201.compute-1.amazonaws.com',
+  port: 5432,
+  username: 'iymdszhwzlyzgn',
+  password: '64b1217aa195e15521db5a62e156270a8fbf1f772c7c47ef230304944a8fec7b',
+  database: 'dds5891q79rad0',
   define: {
     timestamps: true,
     undescored: true,
